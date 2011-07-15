0920642e7a04fe4784aeee9ccc1d03b89fb58db3
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-15 14:05:41 -0400

    Added commenting.  Removed the config file generator for now.

--- lib/generators/acts_as_multipart_form/install_generator.rb
+++ lib/generators/acts_as_multipart_form/install_generator.rb
@@ -5,6 +5,9 @@ require 'rails/generators/active_record'
 
 module ActsAsMultipartForm
   module Generators
+    # Generator for copying the InProgressForm model migration and an initializer to the including project
+    #
+    # @author Jeremiah Hemphill
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
 
@@ -16,6 +19,7 @@ Description:
 DESC
 
       # Implement the required interface for Rails::Generators::Migration.
+      # thanks Kaminari gem for this bit of code
       def self.next_migration_number(dirname) #:nodoc:
         next_migration_number = current_migration_number(dirname) + 1
         if ActiveRecord::Base.timestamped_migrations
@@ -25,10 +29,13 @@ DESC
         end
       end
 
+      # Creates the config file
+      # Commented out until the config actually has information
       def copy_config_file
-        copy_file "config.rb", "config/initializers/acts_as_multipart_form.rb"
+        #copy_file "config.rb", "config/initializers/acts_as_multipart_form.rb"
       end
 
+      # Creates the migration
       def copy_migration
         migration_template "migrations/install_migration.rb", "db/migrate/create_multipart_form_tables.rb"
       end
