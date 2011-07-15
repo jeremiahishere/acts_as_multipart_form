ba83ee5263e35d344725d522b3e3dbb93eb40ae1
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-15 10:01:19 -0400

    Working on speccing and commenting.

--- app/models/multipart_form/in_progress_form.rb
+++ app/models/multipart_form/in_progress_form.rb
@@ -1,4 +1,13 @@
 module MultipartForm
+  # Stores information about multipart forms in progress
+  # Useful when displaying information about the form especially when a user
+  # stops halfway through a form and comes back to it
+  #
+  # form_subject is a polymorphic relationship to the model that uses the form
+  #
+  # form_name relates to the name of the form on the model on the line
+  # acts_as_multipart_form :form_name => {:type => ...
+  # @author Jeremiah Hemphill
   class InProgressForm < ActiveRecord::Base
     belongs_to :form_subject, :polymorphic => true
 
--- spec/in_progress_form_spec.rb
+++ spec/in_progress_form_spec.rb
@@ -3,17 +3,41 @@ require 'spec_helper'
 describe MultipartForm::InProgressForm do
   describe "validations" do
     before(:each) do
+      @ip_form = MultipartForm::InProgressForm.new
+      @person = mock_model(Person)
+      @person.stub!(:model).and_return("Person")
+      @person.stub!(:id).and_return(1)
+
+      form_name = :person_form.to_s
+
+      valid_attributes = {
+        :form_subject => @person,
+        :forn_name => "hire_form",
+        :last_completed_step => "personal_info",
+        :completed => "false"
+      }
 
     end
 
+    it "should be valid" do
+      @ip_form.attributes = valid_attributes
+      @ip_form.should be_valid
+    end
 
-    it "should require a form subject"
-    it "should require a form name"
-    it "should require a last completed step"
-    it "should require completed"
-    it "should require the form name be present in the acts_as_multipart_form parameters for the form subject"
+    it "should require a form subject" do
+      @ip_form.should have(1).error_on(:form_subject)
+    end
+
+    it "should require a form name" do
+      @ip_form.should have(1).error_on(:form_name)
+    end
+
+    it "should require a last completed step" do
+      @ip_form.should have(1).error_on(:last_completed_step)
+    end
+
+    it "should require completed" do
+      @ip_form.should have(1).error_on(:completed)
+    end
   end
-
-  it "should default to not complete"
-
 end
