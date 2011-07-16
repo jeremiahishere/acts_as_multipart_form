e623fc3a5f53de90c36df685de5d3530d5e67a09
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-16 11:33:32 -0400

    Started writing spec tests.

--- spec/multipart_form_in_model_spec.rb
+++ spec/multipart_form_in_model_spec.rb
@@ -19,24 +19,68 @@ describe ActsAsMultipartForm::MultipartFormInModel do
     end
 
     it "should set the multipart_forms instance variable if a single symbol is given"
+
+    it "should return if acts_as_multipart_form is called twice"
   end
 
   describe "method_missing instance method" do
-    it "should return true if the method is found"
-    it "should call super if the method is not found"
-    it "should return an error if the method is not found and it is not found in super"
+    before(:each) do
+      @person = Person.new
+    end
+
+    it "should return true if the method is found" do
+      @person.stub!(:multipart_form_action?).and_return(true)
+      @person.some_multipart_form_method.should be_true
+    end
+
+    # I am not convinced that this covers all the test cases
+    # but we are probably fine because we just need to know that super is called
+    it "should call super if the method is not found" do
+      @person.stub!(:multipart_form_action?).and_return(false)
+      lambda { @person.some_multipart_form_method }.should raise_error(NoMethodError)
+    end
   end
 
   describe "respond_to? instance method" do
-    it "should return true if the method is found"
-    it "should call super if the method is not found"
-    it "should return false if the method is not found and it is not found in super"
+    before(:each) do
+      @person = Person.new
+    end
+
+    it "should return true if the method is found" do
+      @person.stub!(:multipart_form_action?).and_return(true)
+      @person.respond_to?(:some_multipart_form_method).should be_true
+    end
+
+    it "should call super if the method is not found" do
+      @person.stub!(:multipart_form_action?).and_return(false)
+      @person.respond_to?(:class).should be_true
+    end
+
+    it "should return false if the method is not found and it is not found in super" do
+      @person.stub!(:multipart_form_action?).and_return(false)
+      @person.respond_to?(:some_multipart_form_method).should be_false
+    end
   end
 
   describe "multipart_form_action? instance method" do
-    it "should return true if the form_name_controller_action matches the multipart_form_controller_action"
-    it "should return false if the multipart_form_controller_action does not start with the form name"
-    it "should call using_multipart_forms?"
+    before(:each) do
+      @person = Person.new
+    end
+
+    it "should call using_multipart_forms?" do
+      @person.should_receive(:using_multipart_forms?)
+      @person.multipart_form_action?(:some_multipart_form_action?)
+    end
+
+    it "should return true if the form_name_controller_action matches the multipart_form_controller_action" do
+      @person.multipart_form_controller_action = "personal_info"
+      @person.multipart_form_action?(:hire_form_personal_info?).should be_true
+    end
+    
+    it "should return false if the multipart_form_controller_action does not start with the form name" do
+      @person.multipart_form_controller_action = "personal_info"
+      @person.multipart_form_action?(:other_form_personal_info?).should be_true
+    end
   end
 
   describe "reset_multipart_form_controller_action method" do
