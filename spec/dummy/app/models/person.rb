56bce71c5098d6ef0b25dcbbe41300808a38831a
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-16 12:28:47 -0400

    Realized I need to redo the method_missing method to first check for form types, then action to avoid calling super.  See failing tests.

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   acts_as_multipart_form :hire_form
+
+  validates_presence_of :name, :if => :hire_form_personal_info?
 end
--- spec/multipart_form_in_model_integration_spec.rb
+++ spec/multipart_form_in_model_integration_spec.rb
@@ -13,4 +13,18 @@ describe Person do
   it "should return true for hire_form_personal_info" do
     @person.hire_form_personal_info?.should be_true
   end
+
+  it "should call the name validation if the action is set" do
+    @person.should have(1).error_on(:name)
+  end
+
+  it "should not call the name validtion if the action does not match the if actionon the validation" do
+    @person.multipart_form_controller_action = "job_info"
+    @person.should be_valid
+  end
+
+  it "should not call the name validtion if the action is not set" do
+    @person.multipart_form_controller_action = nil
+    @person.should be_valid
+  end
 end
