b4cfdf081854ba1cf2032bc28944bb7ac5f315df
Jeremiah Hemphill <jeremiah@cloudspace.com>
2011-07-20 18:12:13 -0400

    Generated the config file for the dummy project.

---
+++ spec/dummy/config/initializers/acts_as_multipart_form.rb
@@ -0,0 +1,6 @@
+ActsAsMultipartForm.configure do |config
+  # config.show_completed = false
+  # config.show_incomplete_parts = false
+  # config.use_numbered_parts_on_index = true
+  # config.show_previous_next_links = true
+end
