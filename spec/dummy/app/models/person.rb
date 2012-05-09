67de60eef2f562a7035dc6484649bc17b8e6551e
Jeremiah Hemphill <jeremiah@cloudspace.com>
2012-05-09 15:14:18 -0400

    Added attr_accessible to in_progress_form.  Big testing overhaul.

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
   multipart_formable :forms => [:hire_form]
 
+  attr_accessible :name
+
   validates_presence_of :name, :if => :hire_form_person_info_update?
 end
